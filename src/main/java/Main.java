145f4e9c7fcc21b1e334f444c27af55cdf69edb1
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-03-14 16:29:13 +0100

    Added the condition of positif character group

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -27,20 +27,45 @@ public class Main {
 
     // for the digit class
     if (pattern.equals("\\d")) {
-        for(int i = 0 ; i < inputLine.length() ; i++){
+        for (int i = 0; i < inputLine.length(); i++) {
             char character = inputLine.charAt(i);
-            if(Character.isDigit(character)){
-                return true ;
+            if (Character.isDigit(character)) {
+                return true;
             }
         }
-        return false ;
+        return false;
+    }
+    // so i can also do it with the same logic before
+
+//    else if(pattern.equals("//w")){
+//        for(int i = 0 ; i < pattern.length() ; i ++){
+//            char character = inputLine.charAt(i);
+//            if(Character.isAlphabetic(character)){
+//                return true ;
+//            }
+//        }
+//        return false ;
+//
+//    }
+
+
 
     // for the character class so i guess i can do it with regular expression
-    } else if ("\\w".equals(pattern)) {
+     else if ("\\w".equals(pattern)) {
         return inputLine.matches(".*\\w.*");
-    }
 
-    else if (pattern.length() == 1) {
+    //  for the positive character group
+    } else if (pattern.startsWith("[") && pattern.endsWith("]")) {
+         String subStr = pattern.substring(1, pattern.length() -1) ;
+         for(int i = 0 ; i < inputLine.length() ; i++){
+             char character = inputLine.charAt(i);
+             if(subStr.indexOf(character) != -1){
+                 return true ;
+             }
+         }
+         return false ;
+
+    } else if (pattern.length() == 1) {
         return inputLine.contains(pattern) ;
 
     } else {
